0dd91fd461dcef02d6f3fc98496d78602a3f17db
Robbin Schepers <Robbin.Schepers@ordina.nl>
2023-04-13 15:55:56 +0200

    feat: Splash screen

---
+++ src/routes/splash/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Poster from '../../lib/components/Poster.svelte';
+</script>
+
+<header class="flex-center relative h-screen w-full bg-shark" id="header">
+	<Poster animated />
+	<div class="z-50 flex max-h-full flex-col justify-center text-center">
+		<h1 class="block w-screen max-w-4xl px-5 py-20">
+				<img src="/logo.svg" alt="Frontend Festival Logo" class="" />
+		</h1>
+	</div>
+</header>
