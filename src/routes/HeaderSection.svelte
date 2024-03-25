9bfc77d287645abae3918caf50eac9c0d9d10f30
Daria Kryvosheieva <Daria.Kryvosheieva@ordina.nl>
2024-03-25 15:42:35 +0100

    fix: changed logo

--- src/routes/HeaderSection.svelte
+++ src/routes/HeaderSection.svelte
@@ -1,32 +1,53 @@
 <script>
 	import Poster from '$lib/components/Poster.svelte';
-	// import TagBlock from '$lib/components/TagBlock.svelte';
+	import TagBlock from '$lib/components/TagBlock.svelte';
 	import { Icon } from 'svelte-awesome';
-	import { chevronDown } from 'svelte-awesome/icons';
+	import { chevronDown, chevronRight } from 'svelte-awesome/icons';
 </script>
 
-<header class="flex-center relative h-[85vh] w-full bg-shark" id="header">
+<header class="flex-center relative w-full bg-shark" id="header">
 	<Poster />
-	<div class="z-50 flex max-h-full flex-col justify-center text-center">
-		<h1 class="block w-screen max-w-4xl px-5 py-20">
-			<a href="https://pythonconferentie.nl">
-				<img src="/logo.svg" alt="Python Conference Logo" class="" />
+	<div class="relative mt-3 w-full">
+		<div class="absolute left-0 right-0">
+			<div class="flex w-full items-center justify-between px-4">
+				<a href="https://pythonconferentie.nl">
+					<img src="/logo-white.png" alt="Python Conference Logo" class="w-20" />
+				</a>
+				<div class="pr-1">
+					<a class=" inline text-white" target="_blank" rel="noreferrer" href="/code-of-conduct"
+						>Code of Conduct<Icon class="ml-4" data={chevronRight} scale={1} /></a
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="mt-16 flex items-center justify-center">
+			<a href="https://pythonconferentie.nl" class="w-96">
+				<img src="/logo-sopra.png" alt="Python Conference Logo" class="" />
 			</a>
-		</h1>
-		<a
-			class="inline"
-			target="_blank"
-			rel="noreferrer"
-			href="https://www.eventbrite.nl/e/tickets-python-conference-utrecht-715964698687"
-		>
-			<!-- <TagBlock class="text-lg" backgroundColor="fountain">
+		</div>
+		<div class="mt-8 flex items-center justify-center">
+			<TagBlock class="text-lg" backgroundColor="fountain">
+				<div class="text-3xl font-bold">
+					PYCON (NL)<span class="px-4">|</span><span class="pr-4 text-ordina">2024</span>
+				</div>
+			</TagBlock>
+		</div>
+
+		<!-- <a
+					class="inline"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.eventbrite.nl/e/tickets-python-conference-utrecht-715964698687"
+				>
+					<TagBlock class="text-lg" backgroundColor="fountain">
 				<div class="flex flex-col">
 					<div>tickets.get_yours_for_<span class="font-bold">free</span>()</div>
 					<div class="text-xs italic">Registration ends October 8th 23:59</div>
 				</div>
-			</TagBlock> -->
-		</a>
-		<div class="flex-center absolute bottom-0 left-0 w-full">
+			    </TagBlock>
+				</a> -->
+		<div class="mt-14 flex justify-center">
 			<a class="mb-10 inline" href="#info">
 				<Icon class="text-white opacity-75" data={chevronDown} scale={3} />
 			</a>
