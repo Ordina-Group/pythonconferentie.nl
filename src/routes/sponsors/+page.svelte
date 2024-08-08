1aedf16ed0fd10dd734aad2b57eb63bc58248041
qam396 <qamar.nazir@ordina.nl>
2024-08-08 15:17:59 +0200

    Top navigation is added New pages agenda, speakers, sponsors, workshop is added

---
+++ src/routes/sponsors/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    import TopNavSection from '../TopNavSection.svelte';
+    import type { PageData } from './$types';
+    import TagBlock from '$lib/components/TagBlock.svelte';
+	import PracticalInformationSection from '../PracticalInformationSection.svelte';
+    
+    export let data: PageData;
+</script>
+
+<div class="bg-western">
+    <header class="flex-center relative w-full bg-shark" id="header">
+        <TopNavSection></TopNavSection>
+    </header>
+    <div class="m-8 mt-20 py-6">
+        <div class="grid m-auto w-full max-w-6xl text-xl text-black">
+            <div>
+                <TagBlock class="float-left max-w-lg" direction="right" extend={true} backgroundColor="north3">
+                    2024 is the first year PyCon NL will be organized. Previous years “Python Conference” was organized where 150 attendees visited the conference. Our care for quality resulted in an amazing community where we kept growing. This year, the “PythonConference” will be rebranded into PyCon NL where we will begrowing even more.
+                </TagBlock>
+            </div>
+
+            <div>		
+                <TagBlock
+                class="float-right max-w-lg "
+                direction="left"
+                backgroundColor="north2"
+                extend={true}
+                >
+                The reason for sponsoring PyCon NL is to help the community grow, sponsors are what makes this conference possible. By having sponsors, we can keep our prices low. Therefore, this conference can be accessible to everyone interested.
+            </TagBlock></div>
+
+            <div class="mt-4 flex items-center justify-center">
+                <a 
+                    href="https://ordinaweb-my.sharepoint.com/:b:/g/personal/eva_munscher_ordina_nl/EfPnNVqX-bVHo0CbAJr5lsIBV5yL4pmTfFfAVSOE21gj3g?e=ktgCbf">
+                    <TagBlock class="text-lg" backgroundColor="north">
+                        <div class="hover:scale-105 font-bold">
+                            Download <span class="px-4">|</span><span class="text-ordina">PYCON NL Sponsoship brochure</span>
+                        </div>
+                    </TagBlock>
+                </a>
+            </div>
+            
+            <div class="mt-8">
+                <p>If you are interested in participating or have any questions, please get in touch with:</p>
+
+                <address class="italic">
+                    <p>Eva Munscher</p>
+    
+                    <p>Esther Roghair</p>
+    
+                    <a href="mailto:pycon.netherlands@gmail.com">pycon.netherlands@gmail.com</a><br />
+                </address>
+            </div>
+
+            <div class="mt-8 ">
+                <h1 class="font-bold text-3xl text-center">Sponsors 2024</h1>
+
+                <div class='border border-black relative mt-12 text-center'>
+                    <div class="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl pb-8">
+                        <div class="p-6 text-center">
+                          <h4 class="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                            Title sponsor
+                          </h4>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <a href="https://www.soprasteria.com/" target="_blank">
+                                <img src="/marketing/LOGO_SOPRA_STERIA_COLORS_RVB.jpg" alt="Python Conference Logo" class="w-96" />
+                            </a>
+                        </div>
+                      </div>
+
+                    <h2 class="mt-8 font-bold">Gold</h2>
+                    <div></div>
+                    <h2 class="mt-8 font-bold">Silver</h2>
+                    <div></div>
+                    <h2 class="mt-8 font-bold">Bronze</h2>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <PracticalInformationSection></PracticalInformationSection>
+</div>
